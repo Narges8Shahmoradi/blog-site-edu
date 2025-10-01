a60b29e07fb2cc9bb1c2fa3db1df34cecf1bda3a
Narges Shahmoradi <Narcissus.nsh@gmail.com>
2025-10-02 00:42:50 +0330

    Add: Post & Category

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    slug = models.SlugField(max_length=250, unique_for_date= 'publish')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
+    publish = models.DateField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Category(models.Model):
+    category_name = models.CharField(max_length=20, null=True, blank=True)
+        
+    def __str__(self):
+        return self.category_name   
+        
