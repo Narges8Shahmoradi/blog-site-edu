cb90ce0b015d0003949b1bb220fff82faf048146
Narges Shahmoradi <Narcissus.nsh@gmail.com>
2025-10-15 00:05:12 +0330

    Rename blog app to Main app

--- Main/views.py
+++ Main/views.py
@@ -5,8 +5,8 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, '', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, '', {'post': post})
