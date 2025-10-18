0a48ee70042ec0f1b823ed5b81b6e8a2481fd24d
Narges Shahmoradi <Narcissus.nsh@gmail.com>
2025-10-19 02:15:23 +0330

    Add: nested CommentSerializer & author for post comments

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),  # مسیر جعلی ادمین
-    path('secure-admin/', admin.site.urls),  # مسیر واقعی پنل ادمین
-    path('', include('Main.urls')),          # مسیر اپ blog
-    #path('account/', include('account.urls'))  # مسیر اپ account
-    path('api/', include('Main.api.urls')),  # ارجاع به urls اپ api
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('nargesi-1404-path/', admin.site.urls), 
+    path('', include('Main.urls')),         
+    path('api/', include('Main.api.urls')), 
 ]
